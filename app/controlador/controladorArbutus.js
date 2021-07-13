390516c145d28dc9bc4784100455246f25b6069d
Jose Carlos Esparza Rodriguez <r181308633@utna.edu.mx>
2021-07-12 22:05:22 -0500

    JoseCarlos-01edicion del archivo controladorArbutus

---
+++ app/controlador/controladorArbutus.js
@@ -0,0 +1,34 @@
+const ModeloArbutus =require('../modelos/modelosArbutus');
+
+function index(req,res){
+     // console.log('ok');
+    ModeloArbutus.find({})
+    .then(arbutus=>{
+        if(arbutus.length) return res.status(200).send({arbutus});
+        return res.status(204).send({message:'No hay datos que mostrar'});
+    }).catch(error=>res.status(500).send(error));
+}
+
+function buscar(req,res,next){
+    let consulta={};
+    consulta[req.params.key]=req.params.value;
+    ModeloArbutus.find(consulta).then(arbutus=>{
+        if(!arbutus.length) return next();
+        req.body.arbutus=arbutus;
+        return next();
+    }).catch(error=>{req.body.error=error;
+        next();
+    })
+}
+function mostrar(req,res){
+    if(req.body.error) return res.status(500).send({error});
+    if(!req.body.arbutus) return res.status(404).send({message:'No hay resultados'});
+    let arbutus=req.body.arbutus;
+    return res.status(200).send({arbutus});
+}
+
+module.exports={
+    index,
+    buscar,
+    mostrar
+}
