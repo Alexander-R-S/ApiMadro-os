b050d91da2c124e1b3b3ff697deb9cb4475a27b9
Jose Carlos Esparza Rodriguez <r181308633@utna.edu.mx>
2021-07-12 21:51:48 -0500

    JoseCarlos-Configuracion de app.js instanciando rutas

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 //Instanciar rutas
+const rutasArbutus=require('./rutas/rutasArbutus');
+const rutasComaros=require('./rutas/rutasComaros');
 const app=express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Generar endpoints
-//app.use('/vinos',rutasVinos);
+app.use('/arbutus',rutasArbutus);
+app.use('/comarostaphylis',rutasComaros);
 
 module.exports= app;
