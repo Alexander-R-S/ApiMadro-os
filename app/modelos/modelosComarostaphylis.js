c5f31eac714ea0c997c8cce6cc2083e14b825eee
Alexander-R-S <85037538+Alexander-R-S@users.noreply.github.com>
2021-07-14 17:29:50 -0500

    Modificacion del modelo comarosta

--- app/modelos/modelosComarostaphylis.js
+++ app/modelos/modelosComarostaphylis.js
@@ -1,45 +1,45 @@
 const mongoose = require('mongoose');
 
 const ComarostaSchema = new mongoose.Schema({
-    nombre:{
-        type:String,
-        require:true
+    nombre: {
+        type: String,
+        require: true
     },
-    imagenes:{
-        type:String,
-        require:true
+    imagenes: {
+        type: Object,
+        require: true
     },
-    habito:{
-        type:String,
-        require:true
+    habito: {
+        type: String,
+        require: true
     },
-    corteza_ramas:{
-        type:String,
-        require:true
+    corteza_ramas: {
+        type: String,
+        require: true
     },
-    corteza_ramillas:{
-        type:String,
-        require:true
+    corteza_ramillas: {
+        type: String,
+        require: true
     },
-    peciolos:{
-        type:String,
-        require:true
+    peciolos: {
+        type: String,
+        require: true
     },
-    hojas:{
-        type:String,
-        require:true
+    hojas: {
+        type: String,
+        require: true
     },
-    enves:{
-        type:String,
-        require:true
+    enves: {
+        type: String,
+        require: true
     },
-    flores:{
-        type:String,
-        require:true
+    flores: {
+        type: String,
+        require: true
     },
-    ubicacion:{
-        type:String,
-        require:true
+    ubicacion: {
+        type: Object,
+        require: true
     }
 
 })
