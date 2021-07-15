f3f4c5105c867145d4ac636d15e55a9ab21811c6
Alexander <85037538+Alexander-R-S@users.noreply.github.com>
2021-07-15 13:35:00 -0500

    prueba de estructuras logicas and y or para multiconsultas, rutas con varios valores

--- app/rutas/rutasComarostaphylis.js
+++ app/rutas/rutasComarostaphylis.js
@@ -2,8 +2,9 @@ const express=require('express');
 const Router = express.Router();
 const ControladorComarosta=require('../controlador/controladorComarostaphylis');
 
-
 Router.get('/',ControladorComarosta.index)
-.get('/:key/:value',ControladorComarosta.buscar,ControladorComarosta.mostrar);
+.get('/:key/:value',ControladorComarosta.buscar,ControladorComarosta.mostrar)
+.get('/:key/:value/:key/:value',ControladorComarosta.buscarvarios,ControladorComarosta.mostrar)
+.get('/:key/:value/:key/:value/:key/:value',ControladorComarosta.buscaror,ControladorComarosta.mostrar);
 
 module.exports=Router;
