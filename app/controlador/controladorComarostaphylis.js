db9a6bc4cc3f1d4863f1afdc54adf596dbbebc91
Alexander-R-S <85037538+Alexander-R-S@users.noreply.github.com>
2021-07-16 18:51:25 -0500

    creacion de la funcion buscar por varois parametros en el endpoint comarostaphylis

--- app/controlador/controladorComarostaphylis.js
+++ app/controlador/controladorComarostaphylis.js
@@ -24,23 +24,69 @@ function buscar(req,res,next){
     })
 }
 
-/// PREUBAS DE BUSQUEDAD
-function buscarvarios(req,res,next){
-    let consulta={};
-    consulta[req.params.key] = { '$regex': req.params.value, '$options': 'i' } && { '$regex': req.params.value, '$options': 'i' };
-    ModeloComarosta.find(consulta).then(comarosta=>{
-        if(!comarosta.length) return next();
-        req.body.comarosta=comarosta;
-        return next();
-    }).catch(error=>{req.body.error=error;
-        next();
-    })
+function regex(param){
+    param={ '$regex':param, '$options': 'i' };
+    return param;
 }
 
-function buscaror(req,res,next){
-    let consulta={};
-    consulta[req.params.key] = { '$regex': req.params.value, '$options': 'i' } || { '$regex': req.params.value, '$options': 'i' } || { '$regex': req.params.value, '$options': 'i' };
-    ModeloComarosta.find(consulta).then(comarosta=>{
+function buscarvarios(req,res,next){
+    let nombre,habito,corteza1,corteza2,peciolos,hojas,haz,enves,flores,ubicacion;
+    let query={};
+
+    nombre=req.body.nombre;
+    habito=req.body.habito;
+    peciolos=req.body.peciolos_ramillas;
+    hojas=req.body.hojas;
+    margen=req.body.margen;
+    haz=req.body.haz;
+    enves=req.body.enves;
+    inflorescencia=req.body.inflorescencia;
+    flores=req.body.flores;
+    ubicacion=req.body.ubicacion;
+
+    if (nombre) {
+        nombre=regex(nombre);
+        query.nombre=nombre;
+    }
+    if(habito){
+        habito=regex(habito);
+        query.habito=habito;
+    }
+    if(peciolos){
+        peciolos=regex(peciolos);
+        query.peciolos=peciolos;
+    }
+    if(hojas){
+        hojas=regex(hojas);
+        query.hojas=hojas;
+    }
+    if(margen){
+        margen=regex(margen);
+        query.margen=margen;
+    }
+    if(haz){
+        haz=regex(haz);
+        query.haz=haz;
+    }
+    if(enves){
+        enves=regex(enves);
+        query.enves=enves;
+    }
+    if(inflorescencia){
+        inflorescencia=regex(inflorescencia);
+        query.inflorescencia=inflorescencia;
+    }
+    if(flores){
+        flores=regex(flores);
+        query.flores=flores;
+    }
+    if(ubicacion){
+        ubicacion=regex(ubicacion);
+        query.ubicacion=ubicacion;
+    }
+
+    console.log(query);
+    ModeloComarosta.find(query).then(comarosta=>{
         if(!comarosta.length) return next();
         req.body.comarosta=comarosta;
         return next();
@@ -48,7 +94,6 @@ function buscaror(req,res,next){
         next();
     })
 }
-///
 
 function mostrar(req,res){
     if(req.body.error) return res.status(500).send({error});
@@ -60,7 +105,7 @@ function mostrar(req,res){
 module.exports={
     index,
     buscar,
+    regex,
     buscarvarios,
-    buscaror,
     mostrar
 }
--- app/rutas/rutasComarostaphylis.js
+++ app/rutas/rutasComarostaphylis.js
@@ -3,8 +3,7 @@ const Router = express.Router();
 const ControladorComarosta=require('../controlador/controladorComarostaphylis');
 
 Router.get('/',ControladorComarosta.index)
-.get('/:key/:value',ControladorComarosta.buscar,ControladorComarosta.mostrar)
-.get('/:key/:value/:key/:value',ControladorComarosta.buscarvarios,ControladorComarosta.mostrar)
-.get('/:key/:value/:key/:value/:key/:value',ControladorComarosta.buscaror,ControladorComarosta.mostrar);
+.post('/',ControladorComarosta.buscarvarios,ControladorComarosta.mostrar)
+.get('/:key/:value',ControladorComarosta.buscar,ControladorComarosta.mostrar);
 
 module.exports=Router;
