e4d1e9f00638a300892600a73375e4d428de8973
Jose Carlos Esparza Rodriguez <r181308633@utna.edu.mx>
2021-07-30 13:29:41 -0500

    JoseCarlos-Edicion archivos carpeta fetch

--- fetch/js/fetch.js
+++ fetch/js/fetch.js
@@ -1,97 +1,135 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+
 
 	function fetch1() {
 		fetch('http://localhost:3000/arbutus', {
-  		method: 'GET',
-  		//body: JSON.stringify(data)
+			method: 'GET',
+			//body: JSON.stringify(data)
 		}).then(res => res.json())
-		.then(function(data) {
-			var cont="";
-			console.log(data);
-			alert(data.arbutus.length);
-			data.arbutus.forEach(function(dato){ 
-				cont+='<p>'+dato.imagenes.imagen1+'</p>';	
+			.then(function (data) {
+				var cont = "";
+				console.log(data);
+				alert(data.arbutus.length);
+				data.arbutus.forEach(function (dato) {
+					cont += '<p>' + dato.imagenes.imagen1 + '</p>';
+				});
+				document.getElementById("cont1").innerHTML = cont;
+
+			}).catch(function (error) {
+				console.log(error);
 			});
-			document.getElementById("cont1").innerHTML=cont;
-			
-		}).catch(function(error) {
-  			console.log(error);
-		});
 	}
 
 
 	function fetch2() {
 		fetch('http://localhost:3000/comarosta', {
-  		method: 'GET',
-  		//body: JSON.stringify(data)
+			method: 'GET',
+			//body: JSON.stringify(data)
 		}).then(res => res.json())
-		.then(function(data) {
-			var cont="";
-			console.log(data);
-			alert(data.comarosta.length);
-			data.comarosta.forEach(function(dato){ 
-				cont+='<p>'+dato.nombre+'</p>';	
+			.then(function (data) {
+				var cont = "";
+				console.log(data);
+				alert(data.comarosta.length);
+				data.comarosta.forEach(function (dato) {
+					cont += '<p>' + dato.nombre + '</p>';
+				});
+				document.getElementById("cont2").innerHTML = cont;
+
+			}).catch(function (error) {
+				console.log(error);
 			});
-			document.getElementById("cont2").innerHTML=cont;
-			
-		}).catch(function(error) {
-  			console.log(error);
-		});
 	}
 
 	function fetchpost(dato) {
 		fetch('http://localhost:3000/arbutus', {
-    	method: "POST",
-    	body: JSON.stringify(dato),
-    		headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
+			method: "POST",
+			body: JSON.stringify(dato),
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
 		}).then(res => res.json())
-		.then(function(data) {
-			var cont="";
-			console.log(data);
-			alert(data.arbutus.length);
-			data.arbutus.forEach(function(dato){ 
-				cont+='<p>'+dato.nombre+'</p>';	
+			.then(function (data) {
+				var cont = "";
+				console.log(data);
+				alert(data.arbutus.length);
+				data.arbutus.forEach(function (dato) {
+					cont += '<p>' + dato.nombre + '</p>';
+				});
+				document.getElementById("cont1").innerHTML = cont;
+
+			}).catch(function (error) {
+				console.log(error);
 			});
-			document.getElementById("cont1").innerHTML=cont;
-			
-		}).catch(function(error) {
-  			console.log(error);
-		});
 
 		fetch('http://localhost:3000/comarosta', {
-    	method: "POST",
-    	body: JSON.stringify(dato),
-    		headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
+			method: "POST",
+			body: JSON.stringify(dato),
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
 		}).then(res => res.json())
-		.then(function(data) {
-			var cont="";
-			console.log(data);
-			alert(data.comarosta.length);
-			data.comarosta.forEach(function(dato){ 
-				cont+='<p>'+dato.nombre+'</p>';	
-			});
-			document.getElementById("cont1").innerHTML=cont;
-			
-		}).catch(function(error) {
-  			console.log(error);
-		});
-	}
-		//alert('hola');
-		//fetch1();
-		//fetch2();
+			.then(function (data) {
+				var cont = "";
+				console.log(data);
+				alert(data.comarosta.length);
+				data.comarosta.forEach(function (dato) {
+					cont += '<p>' + dato.nombre + '</p>';
+				});
+				document.getElementById("cont1").innerHTML = cont;
 
-		$('#buscar').on('click',function(event) {
-			var input= $('#input').val();
-			var obj={};
-			obj.ubicacion=input;
-			fetchpost(obj);
-			//console.log(obj);
+			}).catch(function (error) {
+				console.log(error);
+			});
+	}
+	//alert('hola');
+	//fetch1();
+	//fetch2();
+
+
+//VARIABLES
+	var obj = {};
+	var cont="";
+	var nex="";
+
+	$('.habito2').on('click', function () {
+		var habito = $(this).prop('id');
+		obj.habito = habito;
+	});
+
+	function siguiente() {
+		$('.filtro').each(function (index, element) {
+			if ($(this).prop('hidden', false)) {
+				cont = $(this).prop('id');
+				nex = $("#" + cont).next("row").prop('id');
+			}
 		});
+		return cont,nex;
+	}
+
+
+	$('#next').on('click', function () {
+		siguiente();
+		alert(cont);
+		alert(nex);
+		$('#prev').prop('hidden', false)
+	});
+
+	$('#prev').on('click', function () {
+
+	});
+
+
+
+
+
+	// $('#').on('click',function(event) {
+	// 	var input= $('#input').val();
+	// 	var obj={};
+	// 	obj.ubicacion=input;
+	// 	fetchpost(obj);
+	// 	//console.log(obj);
+	// });
 
 });
