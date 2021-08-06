86180b7b67142e959b7b191643ac85f91f61de33
Jose Carlos Esparza Rodriguez <r181308633@utna.edu.mx>
2021-08-05 20:51:09 -0500

    JoseCarlos-Edicion de conexion

--- app/configuracion/conexion.js
+++ app/configuracion/conexion.js
@@ -5,7 +5,7 @@ module.exports={
     connection:null,
     connect:function(){
         if(this.connection) return this.connection
-        return mongoose.connect(config.BD).then(conexion =>{
+        return mongoose.connect(config.BD,{useNewUrlParser:true,useUnifiedTopology:true}).then(conexion =>{
             this.connection=conexion;
             console.log('Conexion correcta');
         }).catch(error => console.log(error));
