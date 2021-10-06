3d275eeddc94e390cf2a0c8ebf56ba11ec9cdf8b
Jose Carlos Esparza Rodriguez <r181308633@utna.edu.mx>
2021-10-05 22:50:59 -0500

    JoseCarlos-correccion de nombres incompletos en filtro y seccion de historial

--- fetch/js/filtro.js
+++ fetch/js/filtro.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
 
-//VARIABLES
+	//VARIABLES
 	var obj = {};
-	var cont1 = "";
-	var gen="";
+	var gen = "";
 
 	/////FETCH LLENAR TABLA ARBUTUS
 	function fetch1() {
@@ -19,7 +18,7 @@ $(document).ready(function () {
 						'<button type="button" id="' + dato.nombre + '" class="btn btn-primary vera">Ver mas</button>'
 						+ '</td></tr>';
 				});
-				cont1=cont;
+				cont1 = cont;
 				document.getElementById("tarbutus").innerHTML = cont;
 			}).catch(function (error) {
 				console.log(error);
@@ -32,14 +31,13 @@ $(document).ready(function () {
 		}).then(res => res.json())
 			.then(function (data) {
 				var cont = "";
-				//console.log(data);
 				data.comarosta.forEach(function (dato, index) {
 					cont += '<tr><td scope="col">' + (index + 1) + '</td><td scope="col">'
 						+ dato.nombre + '</td><td scope="col">' +
 						'<button type="button" id="' + dato.nombre + '"class="btn btn-primary verc">Ver mas</button>'
 						+ '</td></tr>';
 				});
-				cont1=cont;
+				cont1 = cont;
 				document.getElementById("tcomaros").innerHTML = cont;
 			}).catch(function (error) {
 				console.log(error);
@@ -48,51 +46,51 @@ $(document).ready(function () {
 
 	/////FETCH FILTRO
 	function fetchpost(dato) {
-		if($('#arbutus').prop('checked')==true){
-		fetch('http://localhost:3000/arbutus', {
-			method: "POST",
-			body: JSON.stringify(dato),
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			}
-		}).then(res => res.json())
-			.then(function (data) {
-				var cont = "";
-				data.arbutus.forEach(function (dato, index) {
-					cont += '<tr><td scope="col">' + (index + 1) + '</td><td scope="col">'
-						+ dato.nombre + '</td><td scope="col">' +
-						'<button type="button" id="' + dato.nombre + '" class="btn btn-primary vera">Ver mas</button>'
-						+ '</td></tr>';
+		if ($('#arbutus').prop('checked') == true) {
+			fetch('http://localhost:3000/arbutus', {
+				method: "POST",
+				body: JSON.stringify(dato),
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				}
+			}).then(res => res.json())
+				.then(function (data) {
+					var cont = "";
+					data.arbutus.forEach(function (dato, index) {
+						cont += '<tr><td scope="col">' + (index + 1) + '</td><td scope="col">'
+							+ dato.nombre + '</td><td scope="col">' +
+							'<button type="button" id="' + dato.nombre + '" class="btn btn-primary vera">Ver mas</button>'
+							+ '</td></tr>';
+					});
+					document.getElementById("tarbutus").innerHTML = cont;
+				}).catch(function (error) {
+					alert('No hay especies que cumplan con la solicitud, modifica tu seleccion');
+					//console.log(error);
 				});
-				document.getElementById("tarbutus").innerHTML = cont;
-			}).catch(function (error) {
-				alert('No hay especies que cumplan con la solicitud, modifica tu seleccion');
-				//console.log(error);
-			});
 		}
-		if($('#comarosta').prop('checked')==true){
-		fetch('http://localhost:3000/comarosta', {
-			method: "POST",
-			body: JSON.stringify(dato),
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			}
-		}).then(res => res.json())
-			.then(function (data) {
-				var cont = "";
-				data.comarosta.forEach(function (dato, index) {
-					cont += '<tr><td scope="col">' + (index + 1) + '</td><td scope="col">'
-						+ dato.nombre + '</td><td scope="col">' +
-						'<button type="button" id="' + dato.nombre + '"class="btn btn-primary verc">Ver mas</button>'
-						+ '</td></tr>';
+		if ($('#comarosta').prop('checked') == true) {
+			fetch('http://localhost:3000/comarosta', {
+				method: "POST",
+				body: JSON.stringify(dato),
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				}
+			}).then(res => res.json())
+				.then(function (data) {
+					var cont = "";
+					data.comarosta.forEach(function (dato, index) {
+						cont += '<tr><td scope="col">' + (index + 1) + '</td><td scope="col">'
+							+ dato.nombre + '</td><td scope="col">' +
+							'<button type="button" id="' + dato.nombre + '"class="btn btn-primary verc">Ver mas</button>'
+							+ '</td></tr>';
+					});
+					document.getElementById("tcomaros").innerHTML = cont;
+				}).catch(function (error) {
+					alert('No hay especies que cumplan con la solicitud, modifica tu seleccion');
+					//console.log(error);
 				});
-				document.getElementById("tcomaros").innerHTML = cont;
-			}).catch(function (error) {
-				alert('No hay especies que cumplan con la solicitud, modifica tu seleccion');
-				//console.log(error);
-			});
 		}
 	}
 
@@ -102,7 +100,7 @@ $(document).ready(function () {
 			method: 'GET',
 		}).then(res => res.json())
 			.then(function (data) {
-				//console.log(data);
+				console.log(data);
 				document.getElementById("nom").innerHTML = data.arbutus[0].nombre;
 				document.getElementById("hab").innerHTML = data.arbutus[0].habito;
 				document.getElementById("cr").innerHTML = data.arbutus[0].corteza_ramas;
@@ -159,50 +157,57 @@ $(document).ready(function () {
 	}
 
 	//FUNCION CAMBIO DE COLOR BOTONES FILTRO
-	function color(btn,opt) {
-		if(opt==1){
+	function color(btn, opt) {
+		if (opt == 1) {
 			$(btn).addClass('btn-dark');
 		}
-		if(opt==0){
+		if (opt == 0) {
 			$(btn).removeClass('btn-dark');
-		}	
+		}
 	}
 
+	//FUNCION CAMBIO DE COLOR BOTONES FILTRO
+	function selecciones() {
+		document.getElementById("selecciones").innerHTML = "";
+		Object.entries(obj).map(([token, value]) => {
+			document.getElementById("selecciones").innerHTML += `${token} : ${value}\n`;
+		});
+	}
 
 	//MOSTRAR FILTROS DEPENDIENDO GENERO
 
 	$('.genero').on('click', function () {
-		$('#generos').prop('hidden',true);
+		$('#generos').prop('hidden', true);
 		reset();
-		$('#filtro').prop('hidden',false);
+		$('#filtro').prop('hidden', false);
 		$('.collapse').collapse('hide');
 		$('input[type=checkbox]').not(this).prop('checked', false);
-		 gen = $(this).prop('id');
-		 if(gen=="arbutus"){
+		gen = $(this).prop('id');
+		if (gen == "arbutus") {
 			fetch1();
-			 $('#tablaarb').prop('hidden',false);
-			 $('#tablacoma').prop('hidden',true);
-			 $('#btn-comaros').prop('hidden',true);
-			 $('#btn-arbutus').prop('hidden',false);
-		 }
-		 if(gen=="comarosta"){
+			$('#tablaarb').prop('hidden', false);
+			$('#tablacoma').prop('hidden', true);
+			$('#btn-comaros').prop('hidden', true);
+			$('#btn-arbutus').prop('hidden', false);
+		}
+		if (gen == "comarosta") {
 			fetch2();
-			$('#tablacoma').prop('hidden',false);
-			$('#tablaarb').prop('hidden',true);
-			$('#btn-arbutus').prop('hidden',true);
-			$('#btn-comaros').prop('hidden',false);
+			$('#tablacoma').prop('hidden', false);
+			$('#tablaarb').prop('hidden', true);
+			$('#btn-arbutus').prop('hidden', true);
+			$('#btn-comaros').prop('hidden', false);
 		}
-		if($('#arbutus').prop('checked')==false && $('#comarosta').prop('checked')==false){
-			$('#filtro').prop('hidden',true);
-			$('.tables').prop('hidden',true);
+		if ($('#arbutus').prop('checked') == false && $('#comarosta').prop('checked') == false) {
+			$('#filtro').prop('hidden', true);
+			$('.tables').prop('hidden', true);
 		}
-		
+
 	});
-	
+
 	///FUNCION QUE OCULTA LOS DEMAS FILTROS
 	$('.collapse').on('show.bs.collapse', function () {
 		$('.collapse').not(this).collapse('hide');
-	  });
+	});
 
 	/////////////////CHECKBOXES
 
@@ -211,14 +216,15 @@ $(document).ready(function () {
 		var habito = $(this).prop('name');
 		$('.habito2').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
-			color('#btnhab',0);
+			color('#btnhab', 0);
 			obj.habito = "";
-			fetch1(); fetch2();
+			setTimeout(fetch1, 500); setTimeout(fetch2, 500);
 		} else {
-			color('#btnhab',1);
+			color('#btnhab', 1);
 			obj.habito = habito;
-			fetchpost(obj);
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
@@ -227,13 +233,14 @@ $(document).ready(function () {
 		var corm = $(this).prop('name');
 		$('.corm').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
-			color('#btncortrm',0);
+			color('#btncortrm', 0);
 			obj.corteza_ramas = "";
 		} else {
-			color('#btncortrm',1);
-			obj.corteza_ramas  = corm;
-			fetchpost(obj);
+			color('#btncortrm', 1);
+			obj.corteza_ramas = corm;
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
@@ -242,13 +249,14 @@ $(document).ready(function () {
 		var corll = $(this).prop('name');
 		$('.corll').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
-			color('#btncortrll',0);
+			color('#btncortrll', 0);
 			obj.corteza_ramillas = "";
 		} else {
-			color('#btncortrll',1);
-			obj.corteza_ramillas  = corll;
-			fetchpost(obj);
+			color('#btncortrll', 1);
+			obj.corteza_ramillas = corll;
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
@@ -257,17 +265,18 @@ $(document).ready(function () {
 		var peci = $(this).prop('name');
 		$('.peci').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
-			color('.btnpec',0);
-			if($('#arbutus').prop('checked')==true){obj.peciolos = ""}else{
+			color('.btnpec', 0);
+			if ($('#arbutus').prop('checked') == true) { obj.peciolos = "" } else {
 				obj.peciolos_ramillas = "";
 			}
 		} else {
-			color('.btnpec',1);
-			if($('#arbutus').prop('checked')==true){obj.peciolos = peci}else{
+			color('.btnpec', 1);
+			if ($('#arbutus').prop('checked') == true) { obj.peciolos = peci } else {
 				obj.peciolos_ramillas = peci;
 			}
-			fetchpost(obj);
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
@@ -276,13 +285,14 @@ $(document).ready(function () {
 		var marg = $(this).prop('name');
 		$('.marg').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
-			color('#btnmarg',0);
+			color('#btnmarg', 0);
 			obj.margen = "";
 		} else {
-			color('#btnmarg',1);
-			obj.margen  = marg;
-			fetchpost(obj);
+			color('#btnmarg', 1);
+			obj.margen = marg;
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
@@ -291,13 +301,14 @@ $(document).ready(function () {
 		var inflo = $(this).prop('name');
 		$('.inflo').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
-			color('#btninflo',0);
+			color('#btninflo', 0);
 			obj.inflorescencia = "";
 		} else {
-			color('#btninflo',1);
+			color('#btninflo', 1);
 			obj.inflorescencia = inflo;
-			fetchpost(obj);
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
@@ -306,13 +317,15 @@ $(document).ready(function () {
 		var hoja = $(this).prop('name');
 		$('.hoja').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
-			color('.btnhoj',0);
+			color('.btnhoj', 0);
 			obj.hojas = "";
 		} else {
-			color('.btnhoj',1);
+			color('.btnhoj', 1);
 			obj.hojas = hoja;
-			fetchpost(obj);
+			selecciones();
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
@@ -321,13 +334,14 @@ $(document).ready(function () {
 		var has = $(this).prop('name');
 		$('.has').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
-			color('.btnhaz',0);
+			color('.btnhaz', 0);
 			obj.haz = "";
 		} else {
-			color('.btnhaz',1);
+			color('.btnhaz', 1);
 			obj.haz = has;
-			fetchpost(obj);
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
@@ -336,13 +350,14 @@ $(document).ready(function () {
 		var env = $(this).prop('name');
 		$('.env').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
-			color('.btnenv',0);
+			color('.btnenv', 0);
 			obj.enves = "";
 		} else {
-			color('.btnenv',1);
+			color('.btnenv', 1);
 			obj.enves = env;
-			fetchpost(obj);
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
@@ -352,12 +367,13 @@ $(document).ready(function () {
 		$('.ubi').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
 			obj.ubicacion = "";
-			color('#btnubi',0);
+			color('#btnubi', 0);
 		} else {
-			color('#btnubi',1);
+			color('#btnubi', 1);
 			obj.ubicacion = ubi;
-			fetchpost(obj);
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
@@ -366,22 +382,24 @@ $(document).ready(function () {
 		var flores = $(this).prop('name');
 		$('.flor').not(this).prop('checked', false);
 		if ($(this).prop('checked') == false) {
-			color('.btnflo',0);
+			color('.btnflo', 0);
 			obj.flores = "";
 		} else {
-			color('.btnflo',1);
+			color('.btnflo', 1);
 			obj.flores = flores;
-			fetchpost(obj);
+			setTimeout(fetchpost, 500, obj);
 		}
+		selecciones();
 		console.log(obj);
 	});
 
 	///BOTONES GENEROS
 	$('#btngeneros').on('click', function () {
 		$('input[type=checkbox]').prop('checked', false);
-		$('#filtro').prop('hidden',true);
-		$('#generos').prop('hidden',false);
-		$('.tables').prop('hidden',true);
+		$('#filtro').prop('hidden', true);
+		$('#generos').prop('hidden', false);
+		$('.tables').prop('hidden', true);
+		$('#selecciones').html("");
 		reset();
 	});
 
@@ -389,21 +407,23 @@ $(document).ready(function () {
 	$('#reiniciar').on('click', function () {
 		$('.collapse :checkbox').prop('checked', false);
 		$('.collapse').collapse('hide');
-		if($('#arbutus').prop('checked')==true){fetch1()}
-		if($('#comarosta').prop('checked')==true){fetch2()}
+		if ($('#arbutus').prop('checked') == true) { fetch1() }
+		if ($('#comarosta').prop('checked') == true) { fetch2() }
+		$('#selecciones').html("");
 		reset();
 	});
 
 	///BOTONES VER MAS
 	$('body').on('click', '.vera', function () {
 		var id = $(this).prop('id');
-		fetchfinala(id);
+		//2milis
+		setTimeout(fetchfinala, 500, id);
 		$('#ver1').modal('show');
 	});
 
 	$('body').on('click', '.verc', function () {
 		var id = $(this).prop('id');
-		fetchfinalc(id);
+		setTimeout(fetchfinalc, 500, id);
 		$('#ver2').modal('show');
 	});
 
